455c6bbb7cd725a90cf0d602a5de1b1412d8f71a
pakkapong <champ_rv42@hotmail.com>
2017-11-01 10:13:51 +0700

    add log

--- app.go
+++ app.go
@@ -75,6 +75,7 @@ func VerificationEndpoint(w http.ResponseWriter, r *http.Request) {
 }
 
 func ProcessMessage(event Messaging) {
+	fmt.Println("-----------------------------------------------------"+event.Sender.ID+"-------------------------------------------------------------------")
 	client := &http.Client{}
 	response := Response{
 		Recipient: User{
@@ -108,6 +109,7 @@ func ProcessMessage(event Messaging) {
 func MessagesEndpoint(w http.ResponseWriter, r *http.Request) {
 	var callback Callback
 	json.NewDecoder(r.Body).Decode(&callback)
+	
 	if callback.Object == "page" {
 		for _, entry := range callback.Entry {
 			for _, event := range entry.Messaging {
