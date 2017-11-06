98cda1884d512ac3a2df7ba086d6fcd23fa6897b
pakkapong <champ_rv42@hotmail.com>
2017-11-06 15:03:49 +0700

    temp

--- app.go
+++ app.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	// "bytes"
-	// "encoding/json"
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -77,59 +77,58 @@ func VerificationEndpoint(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// func ProcessMessage(event Messaging) {
-// 	fmt.Println("-----------------------------------------------------ProcessMessage-------------------------------------------------------------------")
-// 	fmt.Println("-----------------------------------------------------"+event.Sender.ID+"-------------------------------------------------------------------")
-// 	client := &http.Client{}
-// 	response := Response{
-// 		Recipient: User{
-// 			ID: event.Sender.ID,
-// 		},
-// 		Message: Message{
-// 			Attachment: &Attachment{
-// 				Type: "image",
-// 				Payload: Payload{
-// 					URL: IMAGE,
-// 				},
-// 			},
-// 		},
-// 	}
-// 	body := new(bytes.Buffer)
-// 	json.NewEncoder(body).Encode(&response)
-// 	url := fmt.Sprintf(FACEBOOK_API, os.Getenv("PAGE_ACCESS_TOKEN"))
-// 	req, err := http.NewRequest("POST", url, body)
-// 	req.Header.Add("Content-Type", "application/json")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
+func ProcessMessage(event Messaging) {
+	fmt.Println("-----------------------------------------------------ProcessMessage-------------------------------------------------------------------")
+	fmt.Println("-----------------------------------------------------"+event.Sender.ID+"-------------------------------------------------------------------")
+	client := &http.Client{}
+	response := Response{
+		Recipient: User{
+			ID: event.Sender.ID,
+		},
+		Message: Message{
+			Attachment: &Attachment{
+				Type: "image",
+				Payload: Payload{
+					URL: IMAGE,
+				},
+			},
+		},
+	}
+	body := new(bytes.Buffer)
+	json.NewEncoder(body).Encode(&response)
+	url := fmt.Sprintf(FACEBOOK_API, os.Getenv("PAGE_ACCESS_TOKEN"))
+	req, err := http.NewRequest("POST", url, body)
+	req.Header.Add("Content-Type", "application/json")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-// 	resp, err := client.Do(req)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	defer resp.Body.Close()
-// }
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+}
 
 func MessagesEndpoint(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("-----------------------------------------------------MessagesEndpoint 55-------------------------------------------------------------------")
 	fmt.Println(r.Body)
-	w.WriteHeader(200)
-	w.Write([]byte("Got your message"))
-	// var callback Callback
-	// json.NewDecoder(r.Body).Decode(&callback)
 	
-	// if callback.Object == "page" {
-	// 	for _, entry := range callback.Entry {
-	// 		for _, event := range entry.Messaging {
-	// 			ProcessMessage(event)
-	// 		}
-	// 	}
-	// 	w.WriteHeader(200)
-	// 	w.Write([]byte("Got your message"))
-	// } else {
-	// 	w.WriteHeader(404)
-	// 	w.Write([]byte("Message not supported"))
-	// }
+	var callback Callback
+	json.NewDecoder(r.Body).Decode(&callback)
+	
+	if callback.Object == "page" {
+		for _, entry := range callback.Entry {
+			for _, event := range entry.Messaging {
+				ProcessMessage(event)
+			}
+		}
+		w.WriteHeader(200)
+		w.Write([]byte("Got your message"))
+	} else {
+		w.WriteHeader(404)
+		w.Write([]byte("Message not supported"))
+	}
 }
 
 func main() {
